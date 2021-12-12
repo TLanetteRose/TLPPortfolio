c1d730c478ad052c89acd83f7bba428f084c2238
T. Lanette <TLPollard4516@gmail.com>
2021-12-12 15:09:51 -0500

    Fixed mobile hamburger menu function
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,7 @@ scrollUp.addEventListener('click', () => {
 
 
 // Nav hamburgerburger selections
-const burger = document.querySelector('#burger-menu');
+/*const burger = document.querySelector('#burger-menu');
 const ul = document.querySelector('nav ul');
 const nav = document.querySelector('nav');
 const navLink = document.querySelector('.nav-link');
@@ -22,3 +22,34 @@ burger.addEventListener('click', () => {
 });
 
 //Close hamburger menu when a link is clicked
+navLink.forEach(
+    function (ul) {
+
+    }
+)*/
+
+const menu = document.querySelector('.nav-items');
+const menuItems = document.querySelectorAll('.nav-link');
+const hamburger = document.querySelector('#burger-menu');
+const closeIcon = document.querySelector('.close-icon');
+const menuIcon = document.querySelector('.menu-icon');
+
+function toggleMenu() {
+    if (menu.classList.contains('show')) {
+        menu.classList.remove('show');
+        closeIcon.style.display = 'none';
+        menuIcon.style.display = 'block';
+    } else {
+        menu.classList.add('show');
+        closeIcon.style.display = 'block';
+        menuIcon.style.display = 'none';
+    }
+}
+
+hamburger.addEventListener('click', toggleMenu);
+
+menuItems.forEach(
+    function (menuItem) {
+        menuItem.addEventListener('click', toggleMenu);
+    }
+)
